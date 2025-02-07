0bcdccbf81dc82f41491fa7186e224b49c564ba7
steven nevins <steven.nevins.js@gmail.com>
2025-02-07 13:02:17 -0500

    test: symoblic user assertion

--- test/WETH9Invariants.sym.sol
+++ test/WETH9Invariants.sym.sol
@@ -8,31 +8,33 @@ import {IWETH} from "../src/IWETH.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract WETH9InvariantsTest is WETH9SymbolicSetup {
+    using Strings for uint256;
+
     uint256 internal constant NUM_USERS = 3;
-    uint256 internal constant NUM_ACTIONS = 3;
+    uint256 internal constant NUM_ACTIONS = 4;
     uint256 internal totalInitialUsersWEth;
     uint256 public totalInitialUserETH;
     uint256 public preconditionWethBalances;
-    /// TODO: Take preconditions as constructor args?
+    address internal symbolicExternalUser;
 
     function setUp() public override {
         super.setUp();
 
-        // Enable symbolic storage for both WETH and harness
+        // Enable symbolic storage for WETH
         svm.enableSymbolicStorage(address(harness.weth()));
 
-        // Create symbolic initial state for non-user balances
+        // Create symbolic initial state for balances of weth
         preconditionWethBalances = svm.createUint256("initial_weth_balance");
+        symbolicExternalUser = svm.createAddress("external_user_address");
         vm.deal(address(weth), preconditionWethBalances);
         harness.initializeGhostVariable(preconditionWethBalances);
 
-        User user;
         for (uint256 i = 0; i < NUM_USERS; i++) {
-            user = createConcreteUser(address(uint160(0x1000 + i)));
+            User user = createConcreteUser(address(uint160(0x1000 + i)));
 
             // Create symbolic initial ETH and WETH balances for each user
             uint256 initialBalance =
-                svm.createUint256(string.concat("initial_user_balance_", Strings.toString(i)));
+                svm.createUint256(string.concat("initial_user_balance_", i.toString()));
             uint256 initialWethBalance = user.getWETHBalance();
 
             vm.deal(address(user), initialBalance);
@@ -41,22 +43,43 @@ contract WETH9InvariantsTest is WETH9SymbolicSetup {
             totalInitialUsersWEth += initialWethBalance;
         }
 
-        // Relate total supply to sum of all balances with overflow protection
-        vm.assume(harness.ghost_totalUserDeposits() <= type(uint256).max);
+        // Ensure the address is not one of our test users
+        for (uint256 i = 0; i < NUM_USERS; i++) {
+            vm.assume(symbolicExternalUser != address(uint160(0x1000 + i)));
+        }
+
+        uint256 externalUserBalance = weth.balanceOf(symbolicExternalUser);
+        uint256 maxAllowedBalance = harness.ghost_totalUserDeposits() - totalInitialUsersWEth;
+
+        // Relate total supply to sum of all balances of users and the universe of users
+        vm.assume(maxAllowedBalance > externalUserBalance);
+        vm.assume(harness.ghost_totalUserDeposits() >= totalInitialUsersWEth);
         vm.assume(harness.ghost_totalUserDeposits() == weth.totalSupply());
+    }
+
+    function check_symbolic_user_balance_fail() public {
+        uint256 symbolicUserBalance = weth.balanceOf(symbolicExternalUser);
+        uint256 totalUserBalances = 0;
+        for (uint256 i = 0; i < NUM_USERS; i++) {
+            address userAddr = address(uint160(0x1000 + i));
+            totalUserBalances += weth.balanceOf(userAddr);
+        }
+        vm.assume(symbolicUserBalance != 0);
+        /// Eq should have counter example - Should only not have counter example for assertLe since there might be multiple sybmolic users with balances
+        uint256 observedUserBalances = totalUserBalances + symbolicUserBalance;
+        assertEq(observedUserBalances, weth.totalSupply(), "Total balances exceed supply");
     }
 
     function check_solvency() public {
         for (uint256 i; i < NUM_ACTIONS; i++) {
             bytes memory data = createWethCalldata();
-            uint256 randIndex = svm.createUint256(string.concat("rand_index", Strings.toString(i)));
+            uint256 randIndex = svm.createUint256(string.concat("rand_index_", i.toString()));
 
             User user = User(getUserAt(randIndex % usersCount()));
 
             uint256 snapshotBefore = svm.snapshotStorage(address(weth));
             if (bytes4(data) == IWETH.deposit.selector) {
-                uint256 value =
-                    svm.createUint256(string.concat("value_action", Strings.toString(i)));
+                uint256 value = svm.createUint256(string.concat("value_action_", i.toString()));
                 user.execute{value: value}(address(harness), data);
             } else {
                 user.execute(address(harness), data);
@@ -66,21 +89,19 @@ contract WETH9InvariantsTest is WETH9SymbolicSetup {
         }
 
         // Check the solvency of deposits
-        uint256 totalSupply = weth.totalSupply();
-        assertEq(address(weth).balance, totalSupply);
+        assertEq(address(weth).balance, weth.totalSupply());
     }
 
     function check_deposits() public {
         for (uint256 i; i < NUM_ACTIONS; i++) {
             bytes memory data = createWethCalldata();
-            uint256 randIndex = svm.createUint256(string.concat("rand_index", Strings.toString(i)));
+            uint256 randIndex = svm.createUint256(string.concat("rand_index_", i.toString()));
             User user = User(getUserAt(randIndex % usersCount()));
 
             // Snapshot and execute with potential value transfer
             uint256 snapshotBefore = svm.snapshotStorage(address(weth));
             if (bytes4(data) == IWETH.deposit.selector) {
-                uint256 value =
-                    svm.createUint256(string.concat("value_action", Strings.toString(i)));
+                uint256 value = svm.createUint256(string.concat("value_action_", i.toString()));
                 user.execute{value: value}(address(harness), data);
             } else {
                 user.execute(address(harness), data);
